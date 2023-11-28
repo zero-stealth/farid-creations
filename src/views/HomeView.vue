a2eb1096831ea9a9af2e630707bde90674634654
zero-stealth <penguincipher@gmail.com>
2023-11-28 12:04:54 +0300

    stable

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <script setup>
 import TopProductComponent from '@/components/TopProductComponent.vue';
 import ProductComponent from '@/components/ProductComponent.vue';
-import AdverBanner from '@/components/AdverBanner.vue';
+import AdverBanner from '../components/AdverBanner.vue';
 import Portfolio from '@/views/PortfolioView.vue';
 import AboutView from '@/views/AboutView.vue';
 </script>
